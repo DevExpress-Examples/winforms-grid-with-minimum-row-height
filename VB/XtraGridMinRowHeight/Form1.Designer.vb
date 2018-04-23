f3dafe1476b8adab516567930ac9129b56dd94ff
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/XtraGridMinRowHeight/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿Namespace XtraGridMinRowHeight
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.btnFont = New System.Windows.Forms.Button()
+			Me.SuspendLayout()
+			' 
+			' btnFont
+			' 
+			Me.btnFont.Dock = System.Windows.Forms.DockStyle.Top
+			Me.btnFont.Location = New System.Drawing.Point(0, 0)
+			Me.btnFont.Name = "btnFont"
+			Me.btnFont.Size = New System.Drawing.Size(284, 23)
+			Me.btnFont.TabIndex = 0
+			Me.btnFont.Text = "Change Font"
+			Me.btnFont.UseVisualStyleBackColor = True
+'			Me.btnFont.Click += New System.EventHandler(Me.btnFont_Click)
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(284, 264)
+			Me.Controls.Add(Me.btnFont)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load)
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private WithEvents btnFont As System.Windows.Forms.Button
+	End Class
+End Namespace
+
---
+++ VB/XtraGridMinRowHeight/MinRowHeightGridView.vb
@@ -0,0 +1,81 @@
+﻿Imports DevExpress.XtraGrid
+Imports DevExpress.XtraGrid.Views.Base
+Imports DevExpress.XtraGrid.Views.Base.Handler
+Imports DevExpress.XtraGrid.Views.Base.ViewInfo
+Imports DevExpress.XtraGrid.Registrator
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
+Imports DevExpress.XtraGrid.Views.Grid.Handler
+Imports System.Drawing
+Imports DevExpress.Utils
+Imports System
+Imports DevExpress.XtraGrid.Drawing
+
+Namespace MinRowHeightXtraGrid
+	Public Class MinRowHeightGridControl
+		Inherits GridControl
+
+		Protected Overrides Sub RegisterAvailableViewsCore(ByVal collection As InfoCollection)
+			MyBase.RegisterAvailableViewsCore(collection)
+			collection.Add(New MyGridViewInfoRegistrator())
+		End Sub
+	End Class
+	Public Class MyGridViewInfoRegistrator
+		Inherits GridInfoRegistrator
+
+		Public Overrides ReadOnly Property ViewName() As String
+			Get
+				Return MinRowHeightGridView.MinRowHeightName
+			End Get
+		End Property
+		Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
+			Return New MinRowHeightGridView(TryCast(grid, GridControl))
+		End Function
+		Public Overrides Function CreateViewInfo(ByVal view As BaseView) As BaseViewInfo
+			Return New MinRowHeightGridViewInfo(TryCast(view, MinRowHeightGridView))
+		End Function
+		Public Overrides Function CreateHandler(ByVal view As BaseView) As BaseViewHandler
+			Return New GridHandler(TryCast(view, GridView))
+		End Function
+	End Class
+
+	Public Class MinRowHeightGridView
+		Inherits GridView
+
+		Public Const MinRowHeightName As String = "MinRowHeightGridView"
+		Public Sub New(ByVal grid As GridControl)
+			MyBase.New(grid)
+		End Sub
+		Public Overrides ReadOnly Property Editable() As Boolean
+			Get
+				Return False
+			End Get
+		End Property
+		Protected Overrides ReadOnly Property ViewName() As String
+			Get
+				Return MinRowHeightName
+			End Get
+		End Property
+	End Class
+	Public Class MinRowHeightGridViewInfo
+		Inherits GridViewInfo
+
+		Public Sub New(ByVal view As MinRowHeightGridView)
+			MyBase.New(view)
+		End Sub
+		Protected Overrides Function CalcMinRowHeight() As Integer
+		   Return Convert.ToInt32(PaintAppearance.Row.CalcTextSize(GInfo.Cache, "Gq", Integer.MaxValue).Width) + 1
+		End Function
+
+			Protected Overrides Function CalcRowCellDrawInfoCore(ByVal ri As GridDataRowInfo, ByVal ci As GridColumnInfoArgs, ByVal cell As GridCellInfo, ByVal nextColumn As GridColumnInfoArgs, ByVal calcEditInfo As Boolean, ByVal nextRow As GridRow, ByVal allowCache As Boolean) As GridCellInfo
+			If ci.Column IsNot Nothing Then
+				cell.CellValueRect.Inflate(0, CellValueVIndent)
+				If calcEditInfo Then
+					CreateCellEditViewInfo(cell, True)
+				End If
+			End If
+			Return MyBase.CalcRowCellDrawInfoCore(ri, ci, cell, nextColumn, calcEditInfo, nextRow, allowCache)
+			End Function
+	End Class
+End Namespace
+
